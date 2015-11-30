f7599521e7bf2e9426d5a5adc9a5a207e78f578a
ilyakorovkin <ilya.korovkin@gmail.com>
2015-11-30 21:06:11 +0800

    awesome new backed
    edit/ and you enter your message

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var app = express()
+
+var message = 'how are you today?'
+
+app.get('/', function (req, res) {
+  res.send(message)
+})
+
+app.get('/edit/:motd', function (req, res) {
+  message = req.params.motd
+  res.send('message updated')
+})
+
+app.listen(3000)
